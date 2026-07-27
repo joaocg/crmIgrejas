c7b2da7825cf2155d23f0c61d766543a05de8eb2
João Coelho <xjogos@gmail.com>
2026-07-27 08:41:32 -0300

    feat: add spa shell route regression

---
+++ tests/Feature/PrimeVueShellTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class PrimeVueShellTest extends TestCase
+{
+    public function test_dashboard_and_login_routes_serve_the_spa_shell(): void
+    {
+        $this->get('/dashboard')->assertOk()->assertSee('<div id="app"></div>', false);
+        $this->get('/login')->assertOk()->assertSee('<div id="app"></div>', false);
+    }
+}
